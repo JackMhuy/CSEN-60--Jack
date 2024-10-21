611eb1b2c272f1b9069f07573d1d44066f0d2163
JackMhuy <mtran3@scu.edu>
2024-10-21 11:31:58 -0700

    added week5 javascr

---
+++ Week5/index.js
@@ -0,0 +1,16 @@
+//console.log("hello world")
+//alert('Hello, World');
+window.document.write('<p>Welcome to CSEN 60</p>');
+// document.append('<p>This should come after h1</p>');
+
+//TODO: fix this function
+
+/**
+ * @description this function add 2 numbers
+ * @param {*} a 
+ * @param {*} b 
+ * @returns  
+ */
+function add(a,b) {
+    reutrn a + b;
+}
