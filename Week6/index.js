e9539ed52ebda470e7199c3961071136e07c69e9
JackMhuy <mtran3@scu.edu>
2024-10-28 11:35:13 -0700

    create addTodo

---
+++ Week6/index.js
@@ -0,0 +1,11 @@
+
+
+
+function addTodo() {
+    console.log("button was clicked")
+    const input = document.getElementById('list-input')
+    console.log("Input value =", input.value)
+
+    const list= document.getElementsByTagName("ul")
+    console.log(list)
+}
