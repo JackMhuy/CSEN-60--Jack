e09f46661e241680faf12943490a064601cf057d
JackMhuy <mtran3@scu.edu>
2024-11-11 11:35:18 -0800

    server class commit

---
+++ Week8/Web Security.md
@@ -0,0 +1,15 @@
+# Web Security Lecture
+## Potential Security Oversight:
+<ul>
+<li>No user authentication
+<li>Allowing weak authentication
+<li>Not sanitizing form entries & requests
+<li>Not encrypting sensitive communications
+<li>Sessions that don’t time out
+<li>Out-of-date (un-patched) software
+<ul>
+<li>New vulnerabilities discovered all the time
+</ul>
+<li>Bugs that expose user data
+</ul>
+
---
+++ Week8/index.js
@@ -0,0 +1,23 @@
+
+function showData() {
+    console.log("show data function finish")
+}
+async function getRandomFact() {
+    const response = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    const data = await response.json()
+    console.log(data.text)
+    //console.log("getData finish")
+}
+//get random fact
+async function getTodayFact(params) {
+    const response = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    const data = await response.json()
+    console.log(data.text)
+    //console.log("getData finish")
+
+}
+
+getRandomFact()
+getTodayFact()
+//getData()
+//showData()
